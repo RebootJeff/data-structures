60eb06e962b1a660d536cacc7ad2c3efa8d4e7d7
Jeff Lee <leejeffc@gmail.com>
2013-12-10 16:29:23 -0800

    Implement BST instantiator, contains method, and depthFirstLog method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,34 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(val){
+  var instance = {};
+  instance.value = val;
+  instance.left = null;
+  instance.right = null;
+  _.extend(instance, bstMethods);
+  return instance;
+};
+
+var bstMethods = {
+  insert: function(val){
+
+  },
+
+  contains: function(val){
+    var result = false;
+
+    if(this.value === val){
+      result = true;
+    } else if(this.value < val){
+      result = result || this.right.contains(val);
+    } else {
+      result = result || this.left.contains(val);
+    }
+
+    return result;
+  },
+
+  depthFirstLog: function(callback){
+    callback(this.value);
+    this.left && this.left.depthFirstLog(callback);
+    this.right && this.right.depthFirstLog(callback);
+  }
 };
