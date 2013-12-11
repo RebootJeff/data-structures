cdc2f5139bc0a58c964b21e29e52c2d06a5c02fd
Jeff Lee <leejeffc@gmail.com>
2013-12-10 16:08:31 -0800

    Add underscore to library. Complete first draft of tree.

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -12,4 +12,22 @@ describe("tree", function() {
   });
 
   // Add more tests here to test the functionality of tree.
+  it("should have a value that can be set", function() {
+    tree.value = 1;
+    expect(tree.value).toEqual(1);
+  });
+
+  it("should have a child after adding it", function() {
+    var child1 = makeTree();
+    child1.value = 1;
+    tree.addChild(child1);
+    expect(tree.children[0].value).toEqual(child1.value);
+  });
+
+  it("should find values using contains", function() {
+    var child1 = makeTree();
+    child1.value = 1;
+    tree.addChild(child1);
+    expect(tree.contains(1)).toBe(true);
+  });
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,14 +2,30 @@ var makeTree = function(){
   var newTree = {};
   newTree.value = undefined;
   newTree.children = undefined;
-
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(childTree){
+  this.children = this.children || [];
+  this.children.push(childTree);
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(val){
+  var result = false;
+
+  var recursiveCall = function(tree){
+    if(tree.value === val){
+      result = true;
+    } else {
+      _.each(tree.children, function(child, index, children){
+        recursiveCall(child);
+      });
+    }
+  };
+
+  recursiveCall(this);
+  return result;
 };
