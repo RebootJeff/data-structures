8b686f6fef491db59e7cf012e4265b5cd852b178
Jeff Lee <leejeffc@gmail.com>
2014-01-11 17:35:13 -0800

    Complete first draft of prefixTree's .insert() method

--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -10,13 +10,15 @@ var Trie = function(key, val, _branch){
 
 Trie.prototype.insert = function(key, val, keyIndex){
 // Add new key-value pair to appropriate branch of the trie.
-  keyIndex = keyIndex || 0;  // keyIndex should only be provided by recursive calls
+  keyIndex = keyIndex || 0;  // `keyIndex` should only be provided by recursive calls.
   if(this.key === key){
     // We've reached a node that already has the given key.
+    // Overwrite original val stored at given key.
     this.val = val;
   } else {
     var child;
     var insertHere = true;
+
     for(var i = 0; i < this.children.length; i++){
       child = this.children[i];
       if(child.branch === key[keyIndex]){
@@ -24,9 +26,23 @@ Trie.prototype.insert = function(key, val, keyIndex){
         insertHere = false;
       }
     }
+
     if(insertHere){
-      // The for-loop didn't find any suitable branches.
+      // The `for`-loop didn't find any suitable branches.
       // Add all necessary nodes to spell out the full key.
+      var currentNode = this;
+      var newNode, subKey, branch;
+      for(i = keyIndex; i < key.length; i++){
+        branch = key.slice(i, i + 1);
+        subKey = key.slice(0, i + 1);
+        if(i === key.length - 1){
+          newNode = new Trie(key, val, branch);
+        } else {
+          newNode = new Trie(subKey, null, branch);
+        }
+        currentNode.children.push(newNode);
+        currentNode = newNode;
+      }
     }
   }
 };
