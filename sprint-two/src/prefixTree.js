64871f6bb913a12619f50741a7f1b1029323ce3e
Jeff Lee <leejeffc@gmail.com>
2014-01-11 17:10:47 -0800

    Check existing branches in .insert() method

--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -1,28 +1,46 @@
-// Prefix trees aka tries aka digital trees aka radix trees
-var Trie = function(key, val){
-// Not all nodes will have a val or children
-// Only root node will have empty key
+// Prefix trees aka tries aka digital trees aka radix trees.
+var Trie = function(key, val, _branch){
+// Not all nodes will have a val or children.
+// Only root node will have empty key.
   this.children = [];
-  this.prefix = null;
+  this.branch = _branch || null;
   this.key = key;
   this.val = val;
 };
 
-Trie.prototype.insert = function(key, val){
-// Add new key-value pair to appropriate branch of the trie
-
+Trie.prototype.insert = function(key, val, keyIndex){
+// Add new key-value pair to appropriate branch of the trie.
+  keyIndex = keyIndex || 0;  // keyIndex should only be provided by recursive calls
+  if(this.key === key){
+    // We've reached a node that already has the given key.
+    this.val = val;
+  } else {
+    var child;
+    var insertHere = true;
+    for(var i = 0; i < this.children.length; i++){
+      child = this.children[i];
+      if(child.branch === key[keyIndex]){
+        child.insert(key, val, keyIndex + 1);
+        insertHere = false;
+      }
+    }
+    if(insertHere){
+      // The for-loop didn't find any suitable branches.
+      // Add all necessary nodes to spell out the full key.
+    }
+  }
 };
 
 Trie.prototype.hasKey = function(key){
-// Return boolean based on weather or not given key is currently stored in the trie
+// Return boolean based on weather or not given key is currently stored in the trie.
 };
 
 Trie.prototype.retrieveVal = function(key){
-// Return val stored with given key
+// Return val stored with given key.
 
 };
 
 Trie.prototype.keysWithPrefix = function(prefix){
-// Return all keys connected to a branch as determined by the given prefix
+// Return all keys connected to a branch as determined by the given prefix.
 
 };
